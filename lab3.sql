deafc735be3bef0fd183acc5446277ad1be07a18
zbytspokojna <abkitsune@gmail.com>
2016-11-10 23:12:22 +0100

    Add files via upload

--- lab3.sql
+++ lab3.sql
@@ -207,6 +207,8 @@ from statystyki;
 \echo </body>
 \echo </html>
 --3.8 make into sql file
-
-
-
+\a
+\pset fieldsep ','
+\t
+select idmeczu, (gospodarze[1] + gospodarze[2] + gospodarze[3] + coalesce(gospodarze[4], 0) + coalesce(gospodarze[5], 0)) as Gosp, (goscie[1]+goscie[2]+goscie[3]+coalesce(goscie[4],0)+coalesce(goscie[5], 0)) as Gosc 
+from statystyki;
