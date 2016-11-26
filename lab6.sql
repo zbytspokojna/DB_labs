b9a9ede7b7fe471a288d0d0eaf538b0767f19aad
zbytspokojna <abkitsune@gmail.com>
2016-11-26 20:03:31 +0100

    Update lab6.sql

--- lab6.sql
+++ lab6.sql
@@ -7,8 +7,7 @@ insert into klienci values
 ('91','Matusiak Alina','Kropiwnickiego 6/3','Leningrad','31-471','031 423 45 38'),
 ('92','Kimono Franek','Karateków 8','Mistrz','30-029','501 498 324');
 --3
-insert into klienci values
-(select '93', 'Matusiak Iza', ulica, miejscowosc, kod, telefon from klienci where nazwa = 'Matusiak Edward');
+insert into klienci select '93', 'Matusiak Iza', ulica, miejscowosc, kod, telefon from klienci where nazwa = 'Matusiak Edward';
 
 --6.2
 --1
